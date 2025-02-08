f68a2aa239b4120555b7f53a38487e031f1aee58
yanagikaito <voltgt777@gmail.com>
2025-02-08 16:52:34 +0900

    プレイヤーのステータス画面に武器と防具を表示させ,名前も表示させました。

--- src/main/java/factory/GameElementFactory.java
+++ src/main/java/factory/GameElementFactory.java
@@ -5,37 +5,9 @@ import java.awt.*;
 
 public abstract class GameElementFactory {
 
-    public abstract JPanel createPanel();
-
-    public abstract JPanel createStartButtonPanel();
-
-    public abstract JPanel createMainTextPanel();
-
-    public abstract JPanel createChoiceButtonPanel();
-
-    public abstract JPanel createPlayerStatusPanel();
-
     public abstract Font createFont();
 
     public abstract Font createNormalFont();
-
-    public abstract JLabel createLabel(String text);
-
-    public abstract JLabel createLvLabelText(String text);
-
-    public abstract JLabel createLvLabelNumber();
-
-    public abstract JLabel createHPLabelText(String text);
-
-    public abstract JLabel createHPLabelNumber();
-
-    public abstract JLabel createATKLabelText(String text);
-
-    public abstract JLabel createATKLabelNumber();
-
-    public abstract JLabel createDEFLabelText(String text);
-
-    public abstract JLabel createDEFLabelNumber();
 
     public abstract JButton createButton(String text);
 
